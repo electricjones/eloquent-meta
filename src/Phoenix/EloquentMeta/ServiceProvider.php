023bb7ffb1dffa7ab66b5341c1a2fa616500a165
chrismichaels84 <chrismichaels84@gmail.com>
2014-08-15 23:42:16 -0500

    update service provider

---
+++ src/Phoenix/EloquentMeta/ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Phoenix\EloquentMeta;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
