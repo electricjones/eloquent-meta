268a76b990bbd3286d6ec37dc641436cb00db2f0
chrismichaels84 <chrismichaels84@gmail.com>
2015-09-29 09:12:24 -0500

    add Helpers tests

---
+++ tests/HelpersTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Phoenix\EloquentMeta\Test;
+
+use Phoenix\EloquentMeta\Helpers;
+use Phoenix\EloquentMeta\Test\Stubs\TestModelForUniqueMetaModel;
+
+class HelpersTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMaybeDecode()
+    {
+        $data = ['a' => 'A', 'true' => true];
+        $validJson = json_encode($data);
+        $actual = Helpers::maybeDecode($validJson);
+        $this->assertEquals(json_decode($validJson, false), $actual, 'failed to decode a valid json string');
+
+        $invalidJson = '{a: }';
+        $actual = Helpers::maybeDecode($invalidJson);
+        $this->assertEquals('{a: }', $actual, 'failed to return invalid json');
+
+        $almostJson = '01.01.1970';
+        $actual = Helpers::maybeDecode($almostJson);
+        $this->assertEquals('01.01.1970', $actual, 'failed to return almost json');
+    }
+}
